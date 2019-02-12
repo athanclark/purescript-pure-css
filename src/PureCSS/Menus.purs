6ab87057ce8915b2946a543fc519c9d31faea4da
Athan Clark <athan.clark@gmail.com>
2019-02-12 13:20:59 -0700

    menus

---
+++ src/PureCSS/Menus.purs
@@ -0,0 +1,42 @@
+module PureCSS.Menus where
+
+import PureCSS.Types (ClassName)
+import Prelude ((<>))
+
+
+pureMenu :: ClassName
+pureMenu = "pure-menu"
+
+pureMenuHeading :: ClassName
+pureMenuHeading = pureMenu <> "-heading"
+
+pureMenuList :: ClassName
+pureMenuList = pureMenu <> "-list"
+
+pureMenuItem :: ClassName
+pureMenuItem = pureMenu <> "-item"
+
+pureMenuLink :: ClassName
+pureMenuLink = pureMenu <> "-link"
+
+pureMenuHorizontal :: ClassName
+pureMenuHorizontal = pureMenu <> "-horizontal"
+
+pureMenuSelected :: ClassName
+pureMenuSelected = pureMenu <> "-selected"
+
+pureMenuDisabled :: ClassName
+pureMenuDisabled = pureMenu <> "-disabled"
+
+
+pureMenuHasChildren :: ClassName
+pureMenuHasChildren = pureMenu <> "-has-children"
+
+pureMenuAllowHover :: ClassName
+pureMenuAllowHover = pureMenu <> "-allow-hover"
+
+pureMenuChildren :: ClassName
+pureMenuChildren = pureMenu <> "-children"
+
+pureMenuScrollable :: ClassName
+pureMenuScrollable = pureMenu <> "-scrollable"
